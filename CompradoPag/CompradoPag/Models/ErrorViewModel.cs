3259b53d33d0baf7c161ea28a6cbc0ad624060fb
Xavier Rosario <xavierr@Xaviers-MacBook-Pro.local>
2019-04-13 22:51:15 -0400

    Proyect Rename

---
+++ CompradoPag/CompradoPag/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CompradoPag.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
